a6ec1b37a8ec11d37632b3e2c6c88b6c30fcd345
afsun7 <omidi.afsaneh7@gmail.com>
2026-06-05 19:14:18 +0330

    checkOtp

--- src/modules/auth/auth.messages.js
+++ src/modules/auth/auth.messages.js
@@ -2,6 +2,10 @@ const AuthMessage = {
   sendOtpSuccessfully: "send otp successfully",
   NotFound: "use not found",
   OtpCodeNotExpired: "otp code not expired please try later",
+  OtpCodeExpired: "otp code expired please try to get new code",
+  OtpCodeIsIncorrect: "otp code is incorrect",
+  LoginSuccessfully: "your login successfully",
+  LogoutSuccessfully: "Successfully logged out",
 };
 module.exports = {
   AuthMessage,
