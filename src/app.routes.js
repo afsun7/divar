fd8e3178f9a40ca8dd1dbaf8e50303b8ed8f46ac
afsun7 <omidi.afsaneh7@gmail.com>
2026-06-18 00:34:14 +0330

    feat: add whoami

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const { authRoutes } = require("./modules/auth/auth.routes");
+const { userRoutes } = require("./modules/user/user.routes");
 
 const mainRouter = Router();
 mainRouter.use("/auth", authRoutes);
+mainRouter.use("/user", userRoutes);
 module.exports = mainRouter;
---
+++ src/modules/user/user.messages.js
@@ -0,0 +1,12 @@
+const UserMessage = {
+  sendOtpSuccessfully: "send otp successfully",
+  NotFound: "use not found",
+  OtpCodeNotExpired: "otp code not expired please try later",
+  OtpCodeExpired: "otp code expired please try to get new code",
+  OtpCodeIsIncorrect: "otp code is incorrect",
+  LoginSuccessfully: "your login successfully",
+  LogoutSuccessfully: "Successfully logged out",
+};
+module.exports = {
+  UserMessage,
+};
