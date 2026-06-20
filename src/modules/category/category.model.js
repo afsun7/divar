817001fc95a8b223626bdadb0255ff40ba5582fc
afsun7 <omidi.afsaneh7@gmail.com>
2026-06-20 21:27:48 +0330

    feat: create model category

---
+++ src/modules/category/category.model.js
@@ -0,0 +1,23 @@
+const { Schema, Types } = require("mongoose");
+
+const CategorySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true, index: true },
+    icon: { type: String, required: true },
+    parent: { type: Types.ObjectId, ref: "Category" },
+    parents: { type: [Types.ObjectId], ref: "Category", default: [] },
+  },
+  {
+    toJSON: { virtuals: true },
+    versionKey: false,
+    id: false,
+  },
+);
+CategorySchema.virtual("children", {
+  ref: "Category",
+  localField: "_id",
+  foreignField: "parent",
+});
+const CategoryModel = model("Category", CategorySchema);
+module.exports = CategoryModel;
