83c2971dbf98810650f35e9d18ee0f8930e8c9c4
afsun7 <omidi.afsaneh7@gmail.com>
2026-06-20 21:27:27 +0330

    feat: create rout category

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { authRoutes } = require("./modules/auth/auth.routes");
 const { userRoutes } = require("./modules/user/user.routes");
+const { categoryRoutes } = require("./modules/category/category.routes");
 
 const mainRouter = Router();
 mainRouter.use("/auth", authRoutes);
 mainRouter.use("/user", userRoutes);
+mainRouter.use("/category", categoryRoutes);
 module.exports = mainRouter;
